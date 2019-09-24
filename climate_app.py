3129daf9dc97dc04aa5716a4f256f0e8c42d87eb
Amairani Garcia <amairany.gg@gmail.com>
2019-09-24 09:41:34 -0700

    Update

--- climate_app.py
+++ climate_app.py
@@ -7,22 +7,14 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
-
-# precipitation = [
-#     {"date": "prcp", "real_name": "Arthur Curry"},
-#     {"superhero": "Batman", "real_name": "Bruce Wayne"},
-#     {"superhero": "Cyborg", "real_name": "Victor Stone"},
-#     {"superhero": "Flash", "real_name": "Barry Allen"},
-#     {"superhero": "Green Lantern", "real_name": "Hal Jordan"},
-#     {"superhero": "Superman", "real_name": "Clark Kent/Kal-El"},
-#     {"superhero": "Wonder Woman", "real_name": "Princess Diana"}
-# ]
+import datetime as dt
 
 # Database Setup
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
+
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
@@ -41,22 +33,30 @@ app = Flask(__name__)
 def home():
     return (
         f"Welcome to the Climate API!<br/>"
+        f"<br/>"
+        f"<br/>"
         f"Available Routes:<br/>"
+        f"<br/>"
+        f"- Dates and temperature observations from the past year<br/>"
         f"/api/v1.0/precipitation<br/>"
-        f"- Dates and temperature observations from the last year<br/>"
-        f"/api/v1.0/stations<br/>"
+        f"<br/>"
         f"- List of stations<br/>"
-        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"<br/>"
         f"- Temperature Observations from the past year<br/>"
-        f"/api/v1.0/<start>"
+        f"/api/v1.0/tobs<br/>"
+        f"<br/>"
         f"- Minimum temperature, the average temperature, and the max temperature for a given start day<br/>"
-        f"/api/v1.0/<start>/<end><br/>"
+        f"/api/v1.0/<start><br/>"
+        f"<br/>"
         f"- Minimum temperature, the average temperature, and the max temperature for a given start-end range<br/>"
+        f"/api/v1.0/<start>/<end><br/>"
     )
 
 
 @app.route("/api/v1.0/precipitation")
 def prcp():
+    # Query results to a Dictionary and return the JSON representation of the dictionary
     # Calculate the date a year ago from the last data point in the database
     a_year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     
@@ -75,11 +75,10 @@ def prcp():
 
     return jsonify(prcp_totals)
 
-
 @app.route("/api/v1.0/stations")
 def stations():
-    """Return a list of all passenger names"""
-    # Query all passengers
+    # Return a JSON list of stations from the dataset
+    # Query stations
     all_stations = session.query(Station.station, Station.name).group_by(Station.station).all()
 
     # Convert list of tuples into normal list
@@ -87,24 +86,36 @@ def stations():
 
     return jsonify(stations_list)
 
-# @app.route("/api/v1.0/tobs")
-# def tobs():
-#     email = "peleke@example.com"
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Return a JSON list of Temperature Observations (tobs) for the previous year
+    # Calculate the date a year ago from the last data point in the database
+    a_year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    
+    # Design a query to retrieve the last 12 months of precipitation data 
+    temp = session.query(Measurement.date, Measurement.tobs).\
+                            filter(Measurement.date >= a_year_ago).\
+                            order_by(Measurement.date.asc()).all()
+    
+    return jsonify(temp)
 
-#     return f"Questions? Comments? Complaints? Shoot an email to {email}."
-
-# @app.route("/api/v1.0/<start>")
-# def start_temp(start):
-#     # get the min/avg/max
-#     temp_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).all()
+@app.route("/api/v1.0/<start>")
+def start_temp(start):
+    # get the min/avg/max
+    temp_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).all()
+    
+    return jsonify(temp_data)
+    
+@app.route("/api/v1.0/<start>/<end>")
+# def range_temp(start, end):
+#  # get the min/avg/max
+#     temp_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(and_(Measurement.date >= start, Measurement.date <= end)).all()
     
 #     return jsonify(temp_data)
+def calc_temps(start_date, end_date):
     
-# @app.route("/api/v1.0/<start>/<end>")
-# def contact():
-#     email = "peleke@example.com"
-
-#     return f"Questions? Comments? Complaints? Shoot an email to {email}."
+    return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
 
 # 4. Define main behavior
 if __name__ == "__main__":
